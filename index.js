5acb95c11dcc257e536c26edabfa633adfc524d7
juan pablo <juanpablovelezsaenz6@gmail.com>
2026-07-02 11:11:17 -0500

    Remove unused service and client routes from index.js

--- index.js
+++ index.js
@@ -12,27 +12,4 @@ app.use('/api/v1', enrutamiento);
 
 app.set('view engine', 'ejs')
 
-// servicios
-
-app.get('/servicios', servicioController.listar);
-
-app.get('/servicios/:nombre', servicioController.consultarServicio);
-
-app.post('/servicios', servicioController.insert);
-
-app.put('/servicios/:nombre', servicioController.update);
-
-app.delete('/servicios/:nombre', servicioController.delete)
-
-//clientes
-
-
-app.get('/clientes/:correo', clienteController.consultarId);
-
-app.post('/clientes', clienteController.insert);
-
-app.put('/clientes/:correo', clienteController.update);
-
-app.delete('/clientes/:correo', clienteController.delete);
-
 app.listen(9800);
